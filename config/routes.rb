323f9f3ed69af98a0a475e83b77d0bcf30c1d580
Matt Chan & Patrick steps-out-for-coffee-on-the-reg Menzel ;) <pair+matt16749.pmmenzel@devbootcamp.com>
2014-08-29 14:57:39 -0700

    Add RESTful routes for Users, Questions, Answers and Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,39 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'questions#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'questions/index' => 'questions#index', as: :questions
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  get 'questions/new' => 'questions#new', as: :new_question
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  post 'questions/create' => 'questions#create'
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  get 'questions/:id' => 'questions#show', as: :question
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  delete 'questions/:id' => 'questions#destroy'
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+  post 'questions/:question_id/answers/create' => 'answers#create'
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+  put 'questions/:question_id/answers/:id/edit' => 'answers#edit'
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  delete 'questions/:question_id/answers/:id' => 'answers#destroy'
+
+  post 'questions/:question_id/answers/:answer_id/comments/create' => 'comments#answer'
+
+  post 'questions/:question_id/comments/create' => 'comments#question'
+
+  delete 'questions/:question_id/comments/:id' => 'comments#destroy'
+
+  get 'users/new' => 'users#new', as: :new_user
+
+  post 'users/create' => 'users#create', as: :users
+
+  get 'users/:id/edit' => 'users#edit', as: :edit_user
+
+  put 'users/:id' => 'users#update'
+
+  get 'users/:id' => 'users#show', as: :user
+
+  delete 'users/:id' => 'users#destroy'
+
 end
