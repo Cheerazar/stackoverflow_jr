3072d14f876cab898adce9ea4e04b54960d6d614
Gunnari Auvinen <gauvinen@gmail.com>
2014-08-29 15:10:37 -0700

    Add basic validations and associations to Answer model.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,7 @@
 class Answer < ActiveRecord::Base
+  validates :body, :user_id, presence: true
+  belongs_to :question
+  belongs_to :user
+  has_many :answer_comments
+  has_many :answer_votes
 end
