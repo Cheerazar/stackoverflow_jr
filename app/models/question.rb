90490ec5a115936fe1fe464dfa34a3ce06133a75
Gunnari Auvinen <gauvinen@gmail.com>
2014-08-29 15:07:31 -0700

    Add associations and basic validations to Question model.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,7 @@
 class Question < ActiveRecord::Base
+  validate :title, :body, :user_id, presence: true
+  belongs_to :user
+  has_many :answers
+  has_many :question_comments
+  has_many :question_votes
 end
