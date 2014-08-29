9f29ca0bffb0f6ba3df3a7418dc7ee35b39faf41
Gunnari Auvinen <gauvinen@gmail.com>
2014-08-29 15:09:02 -0700

    Correct type for validates in Question model.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
-  validate :title, :body, :user_id, presence: true
+  validates :title, :body, :user_id, presence: true
   belongs_to :user
   has_many :answers
   has_many :question_comments
