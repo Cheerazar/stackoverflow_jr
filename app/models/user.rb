ba3f25ea34c216fa93a5ce70035f6492d66de288
Gunnari Auvinen <gauvinen@gmail.com>
2014-08-29 14:33:03 -0700

    Add User model with associations and basic validations for presence of attributes, except for password_hash.

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+	validates :first_name, :last_name, :email, :username, presence: true
+	has_many :questions
+	has_many :answers
+	has_many :question_comments
+	has_many :answer_comments
+	has_many :question_votes
+	has_many :answer_votes
+end
