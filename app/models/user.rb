0a6dac9aef645c92d4de0cfff606fd5fe8766dbd
Gunnari Auvinen <gauvinen@gmail.com>
2014-08-29 14:55:10 -0700

    Change tabs to spaces.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-	validates :first_name, :last_name, :email, :username, presence: true
-	has_many :questions
-	has_many :answers
-	has_many :question_comments
-	has_many :answer_comments
-	has_many :question_votes
-	has_many :answer_votes
+  validates :first_name, :last_name, :email, :username, presence: true
+  has_many :questions
+  has_many :answers
+  has_many :question_comments
+  has_many :answer_comments
+  has_many :question_votes
+  has_many :answer_votes
 end
