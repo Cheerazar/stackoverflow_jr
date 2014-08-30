b62178459c211fe2a0dfc2e520bd0d2525813d79
Gunnari Auvinen <gauvinen@gmail.com>
2014-08-30 14:54:17 -0700

    Correct error in user_factory, where table name was set to user_name instead of username.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     first_name Faker::Name.name.split(' ')[0]
     last_name Faker::Name.name.split(' ')[0]
     email Faker::Internet.email
-    user_name Faker::Internet.user_name
+    username Faker::Internet.user_name
     password_hash 'tested'
   end
 end
