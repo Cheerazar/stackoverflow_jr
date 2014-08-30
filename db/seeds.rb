070240da39ea5c8ac14a13e84b64ecf4443331a0
Gunnari Auvinen <gauvinen@gmail.com>
2014-08-30 11:46:57 -0700

    Add initial user seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user_ids = 20
+question_ids = 200
+question_comment_ids = 200
+answer_ids = 200
+answer_comment_ids = 200
+
+user_ids.times do
+  fake_first_name, fake_last_name = Faker::Name.name.split(' ')
+  User.create(  first_name: fake_first_name,
+                last_name: fake_last_name,
+                email: Faker::Internet.email,
+                username: Faker::Internet.user_name,
+                password_hash: 'tested'
+                )
+end
